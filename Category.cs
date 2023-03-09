c96f1439c4f4f644850d5f93565af088dcc6ace0
hemal14 <122015637+hemal14@users.noreply.github.com>
2023-03-09 16:31:38 +0530

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+       // [Range( 1, 100, ErrorMessage ="Display Order must be between 1 and 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+
+    }
+}
