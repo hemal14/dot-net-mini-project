68312fc417ba632334023d3debf5a6bbf0e23638
hemal14 <122015637+hemal14@users.noreply.github.com>
2023-03-09 16:29:28 +0530

    Add files via upload
    Data

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+
+            public DbSet<Category> Categories { get; set; }
+    
+
+    }
+}
